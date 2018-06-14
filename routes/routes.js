8be3527db5470fa46b1e9b3f035c4818d3ac0e76
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-14 18:45:59 +1200

    some changes

---
+++ routes/routes.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const db = require('../db')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.getUsers()
+    .then(//// => {
+      res.render('/partials/index')
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.get('/', (req, res) => {
+  db.getUsers()
+    .then(users => {
+      res.render('/partials/')
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.get('/', (req, res) => {
+  db.getUsers()
+    .then(users => {
+      res.render('/partials/index')
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.get('/', (req, res) => {
+  db.getUsers()
+    .then(users => {
+      res.render('/partials/index')
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+module.exports = router
