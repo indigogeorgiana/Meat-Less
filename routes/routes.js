3583391741a271834f3f0caebef1eaaa994ab974
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-14 19:05:54 +1200

    get history and post home routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-// const db = require('../db')
+const db = require('../db')
 
 const router = express.Router()
 
@@ -8,40 +8,24 @@ router.get('/', (req, res) => {
   res.render('partials/index')
 })
 
+router.get('/history', (req, res) => {
+  res.render('partials/history')
+})
+
+router.post('/home', (req, res) => {
+  db.getUsers()
+    .then(results => {
+      res.render('partial/home', results)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
 // router.get('/', (req, res) => {
 //   db.getUsers()
 //     .then(results => {
 //       res.render('/partials/index', results)
-//     })
-//     .catch(err => {
-//       res.status(500).send('DATABASE ERROR: ' + err.message)
-//     })
-// })
-
-// router.get('/', (req, res) => {
-//   db.getUsers()
-//     .then(users => {
-//       res.render('/partials/')
-//     })
-//     .catch(err => {
-//       res.status(500).send('DATABASE ERROR: ' + err.message)
-//     })
-// })
-
-// router.get('/', (req, res) => {
-//   db.getUsers()
-//     .then(users => {
-//       res.render('/partials/index')
-//     })
-//     .catch(err => {
-//       res.status(500).send('DATABASE ERROR: ' + err.message)
-//     })
-// })
-
-// router.get('/', (req, res) => {
-//   db.getUsers()
-//     .then(users => {
-//       res.render('/partials/index')
 //     })
 //     .catch(err => {
 //       res.status(500).send('DATABASE ERROR: ' + err.message)
