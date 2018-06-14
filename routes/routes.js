c94e03fba87944a88d1d745141cf5d30ca2f3c11
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-14 18:52:15 +1200

    first route to blank page done

--- routes/routes.js
+++ routes/routes.js
@@ -1,47 +1,51 @@
 const express = require('express')
 
-const db = require('../db')
+// const db = require('../db')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  db.getUsers()
-    .then(//// => {
-      res.render('/partials/index')
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  res.render('partials/index')
 })
 
-router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('/partials/')
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+// router.get('/', (req, res) => {
+//   db.getUsers()
+//     .then(results => {
+//       res.render('/partials/index', results)
+//     })
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
-router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('/partials/index')
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+// router.get('/', (req, res) => {
+//   db.getUsers()
+//     .then(users => {
+//       res.render('/partials/')
+//     })
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
-router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('/partials/index')
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+// router.get('/', (req, res) => {
+//   db.getUsers()
+//     .then(users => {
+//       res.render('/partials/index')
+//     })
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
+
+// router.get('/', (req, res) => {
+//   db.getUsers()
+//     .then(users => {
+//       res.render('/partials/index')
+//     })
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
 module.exports = router
