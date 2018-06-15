dc66d9b83529410152299e55a42201c276a014b8
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-15 13:26:16 +1200

    math function

--- functions/math.js
+++ functions/math.js
@@ -1,17 +1,37 @@
 module.exports = {
-  calcData,
-  lambCarbon,
-  lambWater
+  calcData
+  // lambCarbon,
+  // lambWater
 }
 
 function calcData (data) {
-  return data * 10
+  if (data.meat === 'lamb') {
+    const co2 = data.amount * 0.00392
+    const h2o = data.amount * 7
+    console.log(co2, h2o)
+    return (h2o, co2)
+  } else if (data.meat === 'beef') {
+    const co2 = data.amount * 0.0027
+    const h2o = data.amount * 15.4
+    console.log(co2, h2o)
+    return (h2o, co2)
+  } else if (data.meat === 'pork') {
+    const co2 = data.amount * 0.00121
+    const h2o = data.amount * 6
+    console.log(co2, h2o)
+    return (h2o, co2)
+  } else if (data.meat === 'chicken') {
+    const co2 = data.amount * 0.00069
+    const h2o = data.amount * 4.3
+    console.log(co2, h2o)
+    return (h2o, co2)
+  }
 }
 
-function lambCarbon (amount) {
-  return amount * 0.0392
-}
+// function lambCarbon (amount) {
+//   return amount * 0.0392
+// }
 
-function lambWater (amount) {
-  return amount * 7
-}
+// function lambWater (amount) {
+//   return amount * 7
+// }
