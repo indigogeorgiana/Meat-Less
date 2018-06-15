8168927a2957eb1bfb0bfbc23b1cf3c5ba080cf1
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-15 14:15:58 +1200

    functions for math

--- functions/math.js
+++ functions/math.js
@@ -1,27 +1,28 @@
-module.exports = {
-  calcData
-  // lambCarbon,
-  // lambWater
-}
+module.exports = calcData
 
 function calcData (data) {
-  if (data.meat === 'lamb') {
-    const co2 = data.amount * 0.00392
+  if (data.meat_id === 3) { // lamb
+    const co2 = data.amount * 0.0392
     const h2o = data.amount * 7
     console.log(co2, h2o)
     return (h2o, co2)
-  } else if (data.meat === 'beef') {
-    const co2 = data.amount * 0.0027
+  } else if (data.meat_id === 1) { // beef
+    const co2 = data.amount * 0.027
     const h2o = data.amount * 15.4
     console.log(co2, h2o)
     return (h2o, co2)
-  } else if (data.meat === 'pork') {
-    const co2 = data.amount * 0.00121
+  } else if (data.meat_id === 2) { // pork
+    const co2 = data.amount * 0.0121
     const h2o = data.amount * 6
     console.log(co2, h2o)
     return (h2o, co2)
-  } else if (data.meat === 'chicken') {
-    const co2 = data.amount * 0.00069
+  } else if (data.meat_id === 4) { // goat
+    const co2 = data.amount * 0.015
+    const h2o = data.amount * 1.59
+    console.log(co2, h2o)
+    return (h2o, co2)
+  } else if (data.meat_id === 5) { // poultry
+    const co2 = data.amount * 0.0069
     const h2o = data.amount * 4.3
     console.log(co2, h2o)
     return (h2o, co2)
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,15 +1,111 @@
 const math = require('../functions/math')
 
-test('lambCarbon returns co2 figure per entry', () => {
+test('lambgas returns co2 figure per entry', () => {
+  const data = {
+    meat_id: 3,
+    amount: 100
+  }
   const expected = 3.92
-  const amount = 100
-  const actual = math.lambCarbon(amount)
+  const amount = data
+  const actual = math(amount)
   expect(actual).toBe(expected)
 })
 
-test('lambWater returns water figure per entry', () => {
+test('lambaqua returns water figure per entry', () => {
+  const data = {
+    meat_id: 3,
+    amount: 100
+  }
   const expected = 700
-  const amount = 100
-  const actual = math.lambWater(amount)
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('beefgas returns co2 figure per entry', () => {
+  const data = {
+    meat_id: 'beef',
+    amount: 100
+  }
+  const expected = 2.7
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('beefaqua returns water figure per entry', () => {
+  const data = {
+    meat_id: 'beef',
+    amount: 100
+  }
+  const expected = 1540
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('porkgas returns co2 figure per entry', () => {
+  const data = {
+    meat_id: 'pork',
+    amount: 100
+  }
+  const expected = 1.21
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('porkaqua returns water figure per entry', () => {
+  const data = {
+    meat_id: 'port',
+    amount: 100
+  }
+  const expected = 600
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('poultrygas returns co2 figure per entry', () => {
+  const data = {
+    meat_id: 'poultry',
+    amount: 100
+  }
+  const expected = 0.69
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('poultryaqua returns water figure per entry', () => {
+  const data = {
+    meat_id: 'poultry',
+    amount: 100
+  }
+  const expected = 430
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('goatgas returns co2 figure per entry', () => {
+  const data = {
+    meat_id: 'goat',
+    amount: 100
+  }
+  const expected = 0.69
+  const amount = data
+  const actual = math(amount)
+  expect(actual).toBe(expected)
+})
+
+test('goataqua returns water figure per entry', () => {
+  const data = {
+    meat_id: 'goat',
+    amount: 100
+  }
+  const expected = 430
+  const amount = data
+  const actual = math(amount)
   expect(actual).toBe(expected)
 })
